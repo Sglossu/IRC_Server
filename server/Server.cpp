1b39545e655db4106dba268b5299a426df26d87a
Shasta Glossu <sglossu@ev-d4.kzn.21-school.ru>
2022-03-07 14:48:38 +0300

    1

--- server/Server.cpp
+++ server/Server.cpp
@@ -9,9 +9,41 @@ Server::Server(std::string port, int pass, std::string host_ip) : port(port), pa
 	hints.ai_flags = AI_PASSIVE; // назначение сокету адрес хоста
 }
 
+void Server::print_ip() {
+	for(struct addrinfo *p = serv_addr_info; p != NULL; p = p->ai_next) {
+		void *addr;
+		char *ipver;
+
+		// получаем указатель на адрес, по разному в разных протоколах
+		if (p->ai_family == AF_INET) { // IPv4
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+			addr = &(ipv4->sin_addr);
+			ipver = "IPv4";
+		} else { // IPv6
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
+		}
+		char ipstr[INET6_ADDRSTRLEN];
+		// преобразуем IP в строку и выводим его:
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n", ipver, ipstr);
+	}
+}
+
 void Server::init_server() {
+	if(getaddrinfo(host_ip.c_str(), port.c_str(), &hints, &serv_addr_info))
+	{
+		std::cerr << "selectserver" << std::endl;
+		exit(1);
+	}
 	listener = socket(PF_INET, SOCK_STREAM, 0);
-	getaddrinfo(host_ip.c_str(), port.c_str(), &hints, &res);
+	print_ip();
+
+}
+
+Server::~Server() {
+	freeaddrinfo(serv_addr_info); // todo воткнуть куда-то
 }
 
 void Server::start() {
--- server/Server.hpp
+++ server/Server.hpp
@@ -3,20 +3,24 @@
 #include <string.h>
 #include <iostream>
 #include <netdb.h>
+#include <arpa/inet.h>
 
 class Server {
 private:
 	fd_set			master, read_fds;	// главный сет дескрипторов и временный сет дескрипторов
 	int				fdmax, listener; 				// fdmax - макс число дескрипторов
 	int				newfd; // дескриптор для новых соединений
-	struct addrinfo	hints, *res;
+	struct addrinfo	hints, *serv_addr_info;
 	socklen_t		addrlen;
 	std::string		port;
 	int				pass;
 	std::string		host_ip;
 public:
 	Server(std::string port, int pass, std::string host_ip);
+	~Server();
 
 	void	init_server();
 	void	start();
+private:
+	void	print_ip();
 };
