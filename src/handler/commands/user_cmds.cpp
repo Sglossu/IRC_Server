14eebb32f6a5cdaafc55b3693573149996731ec2
Taras Levko <arheyy@gmail.com>
2022-04-22 00:11:44 +0500

    change msg to send via PRVMSG

--- src/handler/commands/user_cmds.cpp
+++ src/handler/commands/user_cmds.cpp
@@ -1,15 +1,15 @@
 #include "../Handler.hpp"
 
-const std::string Handler::form_privmsg(const Message &raw_msg, const User &sender) {
+const std::string Handler::_form_privmsg(const Message &raw_msg, const User &sender, std::string &receiver_nick) {
     std::string privmsg;
 
-    privmsg = ":" + sender.getNick() + "!" + sender.getUsername() + "@" + _server.ipstr[1] + " PRIVMSG ";
+    privmsg = ":" + sender.getNick() + "!" + sender.getUsername() + "@" + _server.ipstr[0] + " PRIVMSG "; // ipstr[1] isn't corerrect
     // if (raw_msg.get_params()[1].empty()) 
     //     privmsg += ":" + sender.getNick(); 
     // else 
     // под комментом реализация как у Макса с Эриком
-        privmsg += sender.getNick() + " :" + raw_msg.get_params()[1];
-    privmsg += "\r\n";
+        privmsg += receiver_nick + " :" + raw_msg.get_params()[1];
+    privmsg += CR_LF;
     return privmsg;
 }
 
@@ -20,14 +20,25 @@ void    Handler::_cmd_privmsg(Message &msg, User &user) {
 		return _error_msg(user, 411);
     if (msg.get_params().size() < 2)
         return _error_msg(user, 412);
-    std::string         privmsg = form_privmsg(msg, user);
+    
     std::string         receiver_nick;
     std::istringstream  receivers_stream(msg.get_params()[0]);
     std::map<std::string, User *>::iterator it;
     while (getline(receivers_stream, receiver_nick, ',')) {
-        std::cout << receiver_nick << std::endl;
-        it = _server.mapnick_users.find(receiver_nick);
-        if (it != _server.mapnick_users.end())
-            _server.write_to_client(it->second->getFdSock(), privmsg);
+        if (!receiver_nick.size())
+            continue;
+        // std::cout << receiver_nick << std::endl;
+        std::string privmsg = _form_privmsg(msg, user, receiver_nick);
+        std::cout << "formed msg to send| " << privmsg;
+        if (receiver_nick[0] == '@' or receiver_nick[0] == '#') {
+            // todo add send to channel
+        }
+        else if (receiver_nick.size()) {
+            it = _server.mapnick_users.find(receiver_nick);
+            if (it != _server.mapnick_users.end()) {
+                // std::cout << "send prv msg from" << user.getNick() << " to " << it->second->getFdSock() << std::endl;
+                _server.write_to_client(it->second->getFdSock(), privmsg); // хотел сделать отправку по нику но хуй
+            }
+        }
     }
 }
